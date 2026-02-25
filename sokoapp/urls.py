5f6f273c13cf5d43d590d4b524c65fadfa40d8b3
ashanbashi <ashanbashi23@gmail.com>
2026-02-25 16:16:21 +0300

    first commit

--- sokoapp/urls.py
+++ sokoapp/urls.py
@@ -7,10 +7,12 @@ from sokoapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('home/', views.home),
+    path('home/', views.home, name='home'),
 
-    path('contact/', views.contact),
+    path('contact/', views.contact, name='contact'),
 
-    path('about/', views.about),
+    path('about/', views.about, name='about'),
+
+    path('services/', views.services, name='services'),
 
 ]
--- sokoapp/views.py
+++ sokoapp/views.py
@@ -9,3 +9,6 @@ def contact(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
