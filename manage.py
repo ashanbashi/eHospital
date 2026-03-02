1522ad3749b695d74a1c6a926c5a5c3e1490c872
ashanbashi <ashanbashi23@gmail.com>
2026-03-02 15:22:10 +0300

    initial commit

--- manage.py
+++ manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sokohub.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eHospital.settings')
->>>>>>> 830e338 (first commit)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eHospital.settings')                                                                                                                                                                                                                                                                                                                                                                                                                                         
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
